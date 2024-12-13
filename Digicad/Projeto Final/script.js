c34a9442a156477a38534101bcf05a29f8b1fcca
Thiago <thiago_murakami@hotmail.com>
2024-12-13 18:31:29 -0300

    feat: add cursos e functions e afix bug

--- Digicad/Projeto Final/script.js
+++ Digicad/Projeto Final/script.js
@@ -1,13 +1,16 @@
+// function para acessar o form
 function formulario() {  
     // Redireciona para o formulario 
     window.location.href = "./form.html"; 
 }
 
+// function para voltar a pagina inicial
 function voltar() {
     // volta para a pag inicial
     window.location.href = "./index.html"; 
 }
 
+// funciotn de msg do form
 function msg() {
     // declarando variveis
     const nome = document.getElementById('nome').value; 
@@ -22,4 +25,19 @@ function msg() {
         // msg de erro 
         alert("Não foi possivel enviar o Formulário")
     }
+}
+
+// funciotn de saber mais
+function saibaMais() {
+    // Redireciona para o saiba mais 
+    window.location.href = "./cursos.html"; 
+}
+
+// function para enviar email
+function enviarEmail() {
+    const email = 'destinatario@example.com';
+    const subject = 'Assunto do Email';
+    const body = 'Corpo do email aqui.';
+
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 }
