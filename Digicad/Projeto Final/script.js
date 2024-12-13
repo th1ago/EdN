8499347c3decfda7b6eace078885c95076edfc28
Thiago <thiago_murakami@hotmail.com>
2024-12-13 16:50:24 -0300

    feat: criando mais funcoes e arrumando bugs no js

--- Digicad/Projeto Final/script.js
+++ Digicad/Projeto Final/script.js
@@ -1,4 +1,25 @@
-function Formulario() {  
+function formulario() {  
     // Redireciona para o formulario 
     window.location.href = "./form.html"; 
-}    
+}
+
+function voltar() {
+    // volta para a pag inicial
+    window.location.href = "./index.html"; 
+}
+
+function msg() {
+    // declarando variveis
+    const nome = document.getElementById('nome').value; 
+    const email = document.getElementById('email').value; 
+    const telefone = document.getElementById('telefone').value; 
+    const curso = document.getElementById('curso').value; 
+    // verifica se os campos foram preenchidos
+    if (nome && email && telefone && curso) { 
+        // msg de sucesso
+        alert("Formulário enviado com sucesso")
+    } else { 
+        // msg de erro 
+        alert("Não foi possivel enviar o Formulário")
+    }
+}
